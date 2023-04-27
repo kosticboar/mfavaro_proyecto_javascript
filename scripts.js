acb3bc94940323604d93ed770e8b78434da2a153
Martin Favaro <martinfavarob@gmail.com>
2023-04-26 23:46:18 -0300

    Cambiando reglas, 100 jugadores

--- scripts.js
+++ scripts.js
@@ -2,69 +2,80 @@ alert("Bienvenidos al juego de adivina el 2/3 del promedio");
 alert("Cada jugador debe elegir un numero del 1 al 99 con el objetivo de adivinar el 2/3 del promedio de los numeros dados por los  demas jugadores.");
 
 let cantidadJugadores = prompt("Elija la cantidad de jugadores que van a participar. \n Por el momento solamente puede participar 1 jugador", "1");
+let jugadores = [];
 let numeroJugador1;
 let numeroJugador2;
 let ganadasJugador1 = 0;
 let ganadasJugador2 = 0;
 let promedio;
 
-while(isNaN(cantidadJugadores)){
+while (isNaN(cantidadJugadores)) {
     cantidadJugadores = prompt("Ingrese una cantidad de jugadores valida");
 }
 
-cantidadJugadores =  Number(cantidadJugadores);
+cantidadJugadores = Number(cantidadJugadores);
 
-function existeGanador(){
-    if(ganadasJugador1 == 3 || ganadasJugador2 == 3){
+function cargarJugadores(cantJugadoresHumanos) {
+    for (let i = 1; i <= 100; i++) {
+        let newJugador = new Object();
+        if(cantidadJugadoresHumanos > 0){
+            newJugador.nombre = prompt("Ingrese nombre del Jugador " + i);
+            newJugador.tipo = "humano";
+            newJugador.puntaje = 0;
+            newJugador.numero = 0;
+            cantJugadoresHumanos--;
+        }else{
+            newJugador.nombre = "CPU"+i;
+            newJugador.tipo = "cpu";
+            newJugador.puntaje = 0;
+            newJugador.numero = 0;
+        }
+        jugadores.push(newJugador);
+    }
+}
+
+function existeGanador() {
+    if (ganadasJugador1 == 3 || ganadasJugador2 == 3) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-function numberoCPU(min, max){
+function numberoCPU(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function promediarResultados(){
-    return (((numeroJugador1 + numeroJugador2)/2) * 2 )/ 3;
+function promediarResultados() {
+    let sumaTotal = jugadores.reduce((total, jugador) => {
+        return total + jugador.numero;
+    });
+    return ((sumaTotal/jugadores.length) * 2) / 3;
 }
 
 
-function mostrarResultados(){
+function mostrarResultados() {
     return "Jugador 1: " + numeroJugador1 + "\n Jugadro 2: " + numeroJugador2 + "\n Promedio: " + promedio;
-    
+
 }
 
-function compararResultados(){
-    let diferenciaJugador1 = Math.max(numeroJugador1,promedio) - Math.min(numeroJugador1,promedio);
-    let diferenciaJugador2 = Math.max(numeroJugador2,promedio) - Math.min(numeroJugador2,promedio);
-    if(diferenciaJugador1 < diferenciaJugador2){
-        ganadasJugador1++;
-        alert("El ganador de la ronda es el Jugador 1 \n" + mostrarResultados());
-    }else if(diferenciaJugador1 > diferenciaJugador2){
-        ganadasJugador2++;
-        alert("El ganador de la ronda es el Jugador 2 \n" + mostrarResultados());
-    }else{
-        ganadasJugador1++;
-        ganadasJugador2++;
-        alert("Es un empate!!! \n" + mostrarResultados());
-    }
+function compararResultados() {
+ 
 }
 
 
-if(cantidadJugadores == 1){
+if (cantidadJugadores == 1) {
     let rondas = 1;
-    while(!existeGanador()){
-        alert("Round "+ rondas);
+    while (!existeGanador()) {
+        alert("Round " + rondas);
         numeroJugador1 = prompt("Jugador 1, ingresa tu numero");
-        while(isNaN(numeroJugador1)){
+        while (isNaN(numeroJugador1)) {
             numeroJugador1 = prompt("Jugador 1, ingresa tu numero");
         }
         numeroJugador1 = Number(numeroJugador1);
 
         alert("Ahora es el turno de CPU");
-        numeroJugador2 = numberoCPU(1,99);
+        numeroJugador2 = numberoCPU(1, 99);
 
         promedio = promediarResultados();
 
@@ -73,11 +84,11 @@ if(cantidadJugadores == 1){
         numeroJugador1 = 0;
         numeroJugador2 = 0;
     }
-    if(ganadasJugador1>ganadasJugador2){
+    if (ganadasJugador1 > ganadasJugador2) {
         alert("El ganador es el Jugador 1");
-    }else if(ganadasJugador1<ganadasJugador2){
+    } else if (ganadasJugador1 < ganadasJugador2) {
         alert("El ganador es CPU");
-    }else{
+    } else {
         alert("Es un empate, no hay ganadores");
     }
 
