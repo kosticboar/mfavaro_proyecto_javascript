092229bb4e8005b73bf930d17c9a1cf065641846
Martin Favaro <martinfavarob@gmail.com>
2023-05-03 21:26:01 -0300

    Se terminan las ultimas funciones para la 2da entrega, se limita la cantidad de jugadores a 20

--- scripts.js
+++ scripts.js
@@ -1,31 +1,34 @@
 alert("Bienvenidos al juego de adivina el 2/3 del promedio");
 alert("Cada jugador debe elegir un numero del 1 al 99 con el objetivo de adivinar el 2/3 del promedio de los numeros dados por los  demas jugadores.");
 
-let cantidadJugadores = prompt("Elija la cantidad de jugadores que van a participar. \n Por el momento solamente puede participar 1 jugador", "1");
+let cantidadJugadores = prompt("Elija la cantidad de jugadores humanos que van a participar. \n Maximo 20 jugadores", "1");
 let jugadores = [];
-let numeroJugador1;
-let numeroJugador2;
-let ganadasJugador1 = 0;
-let ganadasJugador2 = 0;
+let numeroPartida = [];
 let promedio;
 
-while (isNaN(cantidadJugadores)) {
-    cantidadJugadores = prompt("Ingrese una cantidad de jugadores valida");
+
+function validarCantidadJugadores(cantidadJugadores){
+    if(isNaN(cantidadJugadores)){
+        return true;
+    }else if(Number(cantidadJugadores) > 20 || Number(cantidadJugadores) < 2){
+        return true;
+    }else{
+        return false;
+    }
 }
 
-cantidadJugadores = Number(cantidadJugadores);
-
 function cargarJugadores(cantJugadoresHumanos) {
-    for (let i = 1; i <= 100; i++) {
+    // Carga una lista de jugadores
+    for (let i = 1; i <= 20; i++) {
         let newJugador = new Object();
-        if(cantidadJugadoresHumanos > 0){
+        if (cantJugadoresHumanos > 0) {
             newJugador.nombre = prompt("Ingrese nombre del Jugador " + i);
             newJugador.tipo = "humano";
             newJugador.puntaje = 0;
             newJugador.numero = 0;
             cantJugadoresHumanos--;
-        }else{
-            newJugador.nombre = "CPU"+i;
+        } else {
+            newJugador.nombre = "CPU" + i;
             newJugador.tipo = "cpu";
             newJugador.puntaje = 0;
             newJugador.numero = 0;
@@ -35,62 +38,86 @@ function cargarJugadores(cantJugadoresHumanos) {
 }
 
 function existeGanador() {
-    if (ganadasJugador1 == 3 || ganadasJugador2 == 3) {
+    // Cuando quede 1 solo jugador tendremos 1 ganador
+    if(jugadores.length == 1){
+        alert("El ganador es " + jugadores[0].nombre + " !!!!");
         return true;
-    } else {
+    }else{
         return false;
     }
 }
 
 function numberoCPU(min, max) {
+    // Retorna un numero automatico
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function promediarResultados() {
-    let sumaTotal = jugadores.reduce((total, jugador) => {
-        return total + jugador.numero;
+    // Calcula el 2/3 del promedio
+    let sumaTotal = numeroPartida.reduce((total, numero) => {
+        return total + numero;
     });
-    return ((sumaTotal/jugadores.length) * 2) / 3;
+    return Math.floor(((sumaTotal / jugadores.length) * 2) / 3);
 }
 
 
 function mostrarResultados() {
-    return "Jugador 1: " + numeroJugador1 + "\n Jugadro 2: " + numeroJugador2 + "\n Promedio: " + promedio;
-
+    // todo
 }
 
 function compararResultados() {
- 
-}
-
-
-if (cantidadJugadores == 1) {
-    let rondas = 1;
-    while (!existeGanador()) {
-        alert("Round " + rondas);
-        numeroJugador1 = prompt("Jugador 1, ingresa tu numero");
-        while (isNaN(numeroJugador1)) {
-            numeroJugador1 = prompt("Jugador 1, ingresa tu numero");
+    // Recorre todos los resultados buscando el que mayor diferencia tiene con el promedio y lo retorna
+    return numeroPartida.reduce((numeroPrevio, numero) => {
+        if (Math.abs(numeroPrevio - promedio) > Math.abs(numero - promedio)) {
+            return numeroPrevio;
+        } else if (Math.abs(numeroPrevio - promedio) <= Math.abs(numero - promedio)) {
+            return numero;
         }
-        numeroJugador1 = Number(numeroJugador1);
-
-        alert("Ahora es el turno de CPU");
-        numeroJugador2 = numberoCPU(1, 99);
-
-        promedio = promediarResultados();
-
-        compararResultados();
-        rondas++;
-        numeroJugador1 = 0;
-        numeroJugador2 = 0;
-    }
-    if (ganadasJugador1 > ganadasJugador2) {
-        alert("El ganador es el Jugador 1");
-    } else if (ganadasJugador1 < ganadasJugador2) {
-        alert("El ganador es CPU");
-    } else {
-        alert("Es un empate, no hay ganadores");
-    }
-
+    });
 }
+
+function eliminarPerdedor(numeroPerdedor){
+    // Recorrela lista de jugadores buscando los jugadores que ingresaron el numero perdedor y los elimina de la lista
+    for (let i = 0; i < jugadores.length; i++) {
+        if(jugadores[i].numero == numeroPerdedor){
+            alert("El jugador " + jugadores[i].nombre + " (" + jugadores[i].numero + ")" + " a sido eliminado!!");
+            jugadores.splice(i,1);
+        }
+    }
+}
+
+
+
+//
+while (validarCantidadJugadores(cantidadJugadores)) {
+    cantidadJugadores = prompt("Ingrese una cantidad de jugadores valida");
+}
+
+cantidadJugadores = Number(cantidadJugadores);
+
+// Se carga una lista de jugadores
+cargarJugadores(cantidadJugadores);
+while (!existeGanador()) {
+    // En numeroPartida guardamos todos los numeros ingresados en 1 partida
+    numeroPartida = [];
+    //Se solicita el numero a cada jugador
+    jugadores.map((jugador) => {
+        if (jugador.tipo == "cpu") {
+            jugador.numero = numberoCPU(1, 100);
+        } else {
+            jugador.numero = Number(prompt(jugador.nombre + " ,ingresa tu numero"));
+        }
+        numeroPartida.push(jugador.numero);
+    });
+
+    // Se calcula el 2/3 del promedio
+    promedio = promediarResultados();
+    alert("El promedio es: " + promedio);
+    // Se elimina el perdedor
+    eliminarPerdedor(compararResultados());
+    alert("Los numeros ingresados fueron: " + numeroPartida);
+}
+
+
+
 
